fad13a8e025456758573789884d0fa09451c7676
ministr-kaifa <eyzeep@yandex.ru>
2025-11-11 20:00:45 +0300

    commented out certs for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 import path from 'path';
 import { defineConfig } from 'vite';
 
@@ -11,12 +10,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    https: {
-      key: fs.readFileSync('./certs/privkey.pem'),
-      cert: fs.readFileSync('./certs/fullchain.pem'),
-    },
-    host: '0.0.0.0', // чтобы можно было подключаться с других устройств
-    port: 5173,
-  },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync('./certs/privkey.pem'),
+  //     cert: fs.readFileSync('./certs/fullchain.pem'),
+  //   },
+  //   host: '0.0.0.0', // чтобы можно было подключаться с других устройств
+  //   port: 5173,
+  // },
 });
